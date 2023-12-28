2d8ec0275a9ffb6e2847320cb00b3f681f970d1e
TriBhaskar <panthribhaskar@gmail.com>
2023-12-28 22:37:20 +0530

    Refactor fetching and removing user places

--- src/http.js
+++ src/http.js
@@ -21,3 +21,12 @@ export async function updateUserPlaces(places) {
   }
   return resData.message;
 }
+
+export async function fetchUserPlaces() {
+  const response = await fetch("http://localhost:3000/user-places");
+  const resData = await response.json();
+  if (!response.ok) {
+    throw new Error("Failed to fetch user places.");
+  }
+  return resData.places;
+}
