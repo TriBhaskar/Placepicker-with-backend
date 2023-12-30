7d078217f3b7d1a0ce4f241572f42b49afe8578f
TriBhaskar <panthribhaskar@gmail.com>
2023-12-30 13:50:17 +0530

    Refactor geolocation logic and sort available places by distance

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,29 +1,30 @@
 /* eslint-disable react/prop-types */
-import { useEffect, useState } from "react";
 import Places from "./Places.jsx";
 import Error from "./Error.jsx";
 import { sortPlacesByDistance } from "../loc.js";
 import fetchAvailablesPlaces from "../http.js";
 import useFetch from "../hooks/useFetch.jsx";
 
-// navigator.geolocation.getCurrentPosition((position) => {
-//   console.log(position);
-//   const sortedPlaces = sortPlacesByDistance(
-//     places,
-//     position.coords.latitude,
-//     position.coords.longitude
-//   );
-//   setAvailablePlaces(sortedPlaces);
-//   setIsFetching(false);
-// });
+async function fetchSortedPlaces() {
+  const places = await fetchAvailablesPlaces();
+  return new Promise((resolve) => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      const sortedPlaces = sortPlacesByDistance(
+        places,
+        position.coords.latitude,
+        position.coords.longitude
+      );
+      resolve(sortedPlaces);
+    });
+  });
+}
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const {
     isFetching,
     error,
     fetchedData: availablePlaces,
-    setFetchedData: setAvailablePlaces,
-  } = useFetch(fetchAvailablesPlaces, []);
+  } = useFetch(fetchSortedPlaces, []);
 
   //fetch request using .then() syntax
   // useEffect(() => {
